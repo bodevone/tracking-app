ffb4d5b89a4af061acc92c3602565f998557785b
bodevone <aliby.bbb@gmail.com>
2019-01-05 05:51:15 +0600

    Minor changes to buttons in login and feedback

--- app/src/main/java/com/example/bodevan/trackappfirebase2712/FeedbackFragment.java
+++ app/src/main/java/com/example/bodevan/trackappfirebase2712/FeedbackFragment.java
@@ -5,21 +5,31 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.Html;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class FeedbackFragment extends Fragment {
 
+    private EditText userName;
+    private EditText feedback;
+    private Button send;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_feedback, container, false);
 
+        userName = v.findViewById(R.id.name);
+        feedback = v.findViewById(R.id.feedback);
+
+        //Text colors
         TextView nameView = v.findViewById(R.id.name_color);
         String star = getColoredSpanned("*", "#FF0000");
         String name = getColoredSpanned("Ваше Имя","#666");
@@ -30,11 +40,22 @@ public class FeedbackFragment extends Fragment {
         feedView.setText(Html.fromHtml(star + "" + feed));
 
 
-        Button send = v.findViewById(R.id.bt_send);
+        send = v.findViewById(R.id.bt_send);
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(), "Отзыв отправлен", Toast.LENGTH_LONG).show();
+                String nameUser = userName.getText().toString();
+                String feedbackUser = feedback.getText().toString();
+
+                if (TextUtils.isEmpty(nameUser)) {
+                    Toast.makeText(getActivity(), "Введите Ваше Имя!", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (TextUtils.isEmpty(feedbackUser)) {
+                    Toast.makeText(getActivity(), "Введите Отзыв!", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    Toast.makeText(getActivity(), "Отзыв отправлен", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
