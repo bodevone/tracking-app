061399dd930c27a811656fc8431e93d0b2eb7e93
bodevone <aliby.bbb@gmail.com>
2019-01-06 22:36:51 +0600

    Adding Markers taken from Firebase Database for specific driver

--- app/src/main/java/com/example/bodevan/trackappfirebase2712/MapsUserFragment.java
+++ app/src/main/java/com/example/bodevan/trackappfirebase2712/MapsUserFragment.java
@@ -5,14 +5,13 @@ import android.graphics.drawable.BitmapDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,12 +23,15 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class MapsUserFragment extends Fragment implements OnMapReadyCallback {
 
@@ -39,21 +41,20 @@ public class MapsUserFragment extends Fragment implements OnMapReadyCallback {
 
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDriverLocationsDatabeReference;
-    private ChildEventListener mChildEventListener;
+    private DatabaseReference mDriverPinsDatabaseReference;
 
-    private String username;
     private String driverForUser;
 
     private boolean zoomed = false;
-    private boolean firstPass = true;
 
     final private int height = 500;
     final private int width = 500;
 
     private TextView onlineTime;
-    private Button zoom;
+    private ImageView zoom;
 
     LatLng latLng;
+    ArrayList<LatLng> pins;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -76,6 +77,7 @@ public class MapsUserFragment extends Fragment implements OnMapReadyCallback {
 
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mDriverLocationsDatabeReference = mFirebaseDatabase.getReference().child("driver-locations");
+        mDriverPinsDatabaseReference = mFirebaseDatabase.getReference().child("driver-pins");
 
         Bundle bundle = this.getArguments();
         driverForUser = bundle.getString("driver_for_user");
@@ -91,6 +93,28 @@ public class MapsUserFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        drawPins();
+    }
+
+    public void drawPins() {
+        DatabaseReference ref = mDriverPinsDatabaseReference.child(driverForUser);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    //LatLng location = (LatLng) snapshot.getValue();
+                    double latitude = (double) snapshot.child("latitude").getValue();
+                    double longitude = (double) snapshot.child("longitude").getValue();
+                    //Toast.makeText(getActivity(), String.valueOf(latitude), Toast.LENGTH_LONG).show();
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
     }
 
     private void updateDatabase() {
@@ -102,101 +126,15 @@ public class MapsUserFragment extends Fragment implements OnMapReadyCallback {
                 drawMarker(info.latitude, info.longitude);
                 onlineTime.setText("Водитель был в сети: " + info.timestamp);
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
             }
         };
         mDriverLocationsDatabeReference.child(driverForUser).addValueEventListener(listener);
-
-//        mDriverLocationsDatabeReference.addChildEventListener(new ChildEventListener() {
-//            @Override
-//            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//
-//            }
-//
-//            @Override
-//            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//                //DriverLocation info = dataSnapshot.child(driverForUser).getValue(DriverLocation.class);
-//                Toast.makeText(getActivity(), String.valueOf(dataSnapshot.child(driverForUser).child("latitude").getValue()), Toast.LENGTH_LONG).show();
-//            }
-//
-//            @Override
-//            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-//
-//            }
-//
-//            @Override
-//            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        });
-
-//        Query query = mDriverLocationsDatabeReference.orderByChild("username").equalTo(driverForUser);
-//        query.addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                if (dataSnapshot.exists()) {
-//                    // dataSnapshot is the "issue" node with all children with id 0
-//                    HashMap<String, Object> driverInfo = new HashMap<>();
-////                    driverInfo.put(dataSnapshot.getKey()+"/latitude", lat);
-////                    driverInfo.put(dataSnapshot.getKey()+"/longitude", lon);
-////                    String id = String.valueOf(dataSnapshot);
-////                    Toast.makeText(getActivity(), id, Toast.LENGTH_LONG).show();
-////                    mDriverLocationsDatabeReference.child(username).updateChildren(driverInfo);
-//
-//                    for (final DataSnapshot childSnapshot: dataSnapshot.getChildren()) {
-//                        childSnapshot.getRef().addChildEventListener(new ChildEventListener() {
-//                            @Override
-//                            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//
-//                            }
-//
-//                            @Override
-//                            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//                                double lat = (double) childSnapshot.child("latitude").getValue();
-//                                double lon = (double) childSnapshot.child("longitude").getValue();
-//                                drawMarker(lat, lon);
-//
-//                            }
-//
-//                            @Override
-//                            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-//
-//                            }
-//
-//                            @Override
-//                            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//
-//                            }
-//
-//                            @Override
-//                            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//                            }
-//                        });
-////                        driverInfo.put(childSnapshot.getKey()+"/latitude", lat);
-////                        driverInfo.put(childSnapshot.getKey()+"/longitude", lon);
-//                    }
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        });
     }
 
-
     public void drawMarker(double driverLat, double driverLon) {
-//        if (currentLocationMarker != null) {
-//            currentLocationMarker.remove();
-//        }
         mMap.clear();
         BitmapDrawable bitmapdraw = (BitmapDrawable) getResources().getDrawable(R.drawable.car);
         Bitmap b = bitmapdraw.getBitmap();
@@ -209,7 +147,6 @@ public class MapsUserFragment extends Fragment implements OnMapReadyCallback {
                 .icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
                 .anchor(0.5f, 0.5f).position(currentLocation).title("Driver Location"));
 
-        //currentLocationMarker.setPosition(latLng);
         // Move the camera instantly to hamburg with a zoom of 15.
 
         // Zoom in, animating the camera.
