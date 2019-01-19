fd614d378d2ba702f59e436d782f1ecc3605ca15
bodevone <aliby.bbb@gmail.com>
2019-01-19 17:58:30 +0600

    User sees his location

--- app/src/main/java/com/example/bodevan/trackappfirebase2712/MapsUserFragment.java
+++ app/src/main/java/com/example/bodevan/trackappfirebase2712/MapsUserFragment.java
@@ -1,14 +1,20 @@
 package com.example.bodevan.trackappfirebase2712;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.location.Location;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +23,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -38,12 +49,19 @@ import java.util.Map;
 
 import static android.content.ContentValues.TAG;
 
-public class MapsUserFragment extends Fragment implements OnMapReadyCallback {
+public class MapsUserFragment extends Fragment implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private GoogleMap mMap;
     SupportMapFragment mapFrag;
     Marker currentLocationMarker;
     LatLng latLng;
+
+
+    GoogleApiClient mGoogleApiClient;
+    LocationRequest mLocationRequest;
 
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDriverLocationsDatabeReference;
@@ -51,6 +69,10 @@ public class MapsUserFragment extends Fragment implements OnMapReadyCallback {
 
     private String driverForUser;
     private boolean zoomed = false;
+    private boolean firstTime = false;
+
+    public double myLat;
+    public double myLon;
 
     final private int height = 500;
     final private int width = 500;
@@ -67,6 +89,9 @@ public class MapsUserFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_user_maps, container, false);
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkLocationPermission();
+        }
 
         mapFrag = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFrag == null) {
@@ -92,6 +117,8 @@ public class MapsUserFragment extends Fragment implements OnMapReadyCallback {
         return v;
     }
 
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -112,6 +139,19 @@ public class MapsUserFragment extends Fragment implements OnMapReadyCallback {
         }
 
         drawPins();
+
+        //Initialize Google Play Services
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                buildGoogleApiClient();
+                mMap.setMyLocationEnabled(true);
+            }
+        } else {
+            buildGoogleApiClient();
+            mMap.setMyLocationEnabled(true);
+        }
     }
 
     public void drawPins() {
@@ -141,6 +181,123 @@ public class MapsUserFragment extends Fragment implements OnMapReadyCallback {
         mDriverPinsDatabaseReference.child(driverForUser).addValueEventListener(listener);
     }
 
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(100);
+        mLocationRequest.setFastestInterval(10);
+
+        //TODO: decide on priority to be chosen - might be PRIORITY_HIGH_ACCURACY
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            // ---Get current location latitude, longitude---
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            // Zoom in, animating the camera.
+            if (!zoomed) {
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(12), 2000, null);
+                zoomed = true;
+            }
+
+            myLat = location.getLatitude();
+            myLon = location.getLongitude();
+//            Toast.makeText(getActivity(), "Latitude = " +
+//                            location.getLatitude() + " " + "Longitude = " + location.getLongitude(),
+//                    Toast.LENGTH_LONG).show();
+//
+//            driverLat = location.getLatitude();
+//            driverLon = location.getLongitude();
+//
+//            databaseUpdate(driverLat, driverLon);
+        }
+    }
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                //Prompt the user once explanation has been shown
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted. Do the
+                    // contacts-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(getActivity(),
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        if (mGoogleApiClient == null) {
+                            buildGoogleApiClient();
+                        }
+                        mMap.setMyLocationEnabled(true);
+                    }
+                } else {
+                    // Permission denied, Disable the functionality that depends on this permission.
+                    Toast.makeText(getActivity(), "permission denied", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+            // other 'case' lines to check for other permissions this app might request.
+            // You can add here other case statements according to your requirement.
+        }
+    }
+
+
     private void updateDatabase() {
 
         ValueEventListener listener = new ValueEventListener() {
@@ -159,7 +316,6 @@ public class MapsUserFragment extends Fragment implements OnMapReadyCallback {
     }
 
     public void drawMarker(double driverLat, double driverLon) {
-        //mMap.clear();
         if (currentLocationMarker != null) {
             currentLocationMarker.remove();
         }
@@ -192,6 +348,13 @@ public class MapsUserFragment extends Fragment implements OnMapReadyCallback {
                 }
             });
         }
+
+        if (!firstTime) {
+            Toast.makeText(getActivity(), "My location:" + myLat + myLon, Toast.LENGTH_LONG).show();
+
+            firstTime = true;
+        }
+
     }
 
 }
